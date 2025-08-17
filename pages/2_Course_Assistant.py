d7f69b25cda7ae918c44ff4b3d76fc96837ce7fb
karthig84 <karthig84@gmail.com>
2025-08-17 22:16:13 +0800

    suppress sqlite

--- pages/2_Course_Assistant.py
+++ pages/2_Course_Assistant.py
@@ -3,6 +3,12 @@ from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_chroma import Chroma
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
+import sys
+
+# Force chromadb to use pysqlite3 instead of system sqlite3
+__import__("pysqlite3")
+sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+
 #from dotenv import load_dotenv
 #import os
 
