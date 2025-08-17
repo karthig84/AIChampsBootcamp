52d0e8a350827cb3521b50faac5c8f8b6e3d1c06
karthig84 <karthig84@gmail.com>
2025-08-17 22:20:53 +0800

    update course assistant

--- pages/2_Course_Assistant.py
+++ pages/2_Course_Assistant.py
@@ -1,13 +1,16 @@
+# --- SQLite patch (must come first!) ---
+import sys
+__import__("pysqlite3")
+sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+
 import streamlit as st
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_chroma import Chroma
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
-import sys
 
-# Force chromadb to use pysqlite3 instead of system sqlite3
-__import__("pysqlite3")
-sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+
+
 
 #from dotenv import load_dotenv
 #import os
